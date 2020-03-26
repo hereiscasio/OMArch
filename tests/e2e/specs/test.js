570bc52a4f9e982a491fed6f12a47168d14c5b70
Casio <hereiscasio@gmail.com>
2020-03-26 22:47:49 +0800

    setup circleci + cy + vue

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -4,5 +4,12 @@ describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
     cy.contains('h1', 'Welcome to Your Vue.js App')
+	cy.get('.router--about').click();
+	cy.contains('This is an about page')
+  })
+  it('Visit page: about', () => {
+    cy.visit('/')
+	cy.get('.router--about').click();
+	cy.contains('This is an about page')
   })
 })
